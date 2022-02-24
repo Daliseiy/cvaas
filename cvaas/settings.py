b1f2cb68cc654b20e0b79b392da69d5871569ff8
Daliseiy <44264075+Daliseiy@users.noreply.github.com>
2022-02-25 00:03:51 +0100

    Update settings.py
    Removed secret key

--- cvaas/settings.py
+++ cvaas/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&rh*_glb-t-)f4!==y-g^!an@b-q^yxs3+61!v1n(mu3=ame0t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
